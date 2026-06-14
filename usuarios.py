faaf70b2c9e44ea3ea69ef519a8e4645300c10e0
Mangel-7 <anrogelma@uide.edu.ec>
2026-06-13 19:48:23 -0500

    Refactor user registration with Usuario class

--- usuarios.py
+++ usuarios.py
@@ -1,2 +1,24 @@
-def registrar_usuario(usuarios, usuario):
-    return usuarios + [usuario]
+import uuid
+
+class Usuario:
+    def __init__(self, nombre, correo):
+        self._id = str(uuid.uuid4())
+        self._nombre = nombre
+        self._correo = correo
+
+    def obtener_nombre(self):
+        return self._nombre
+
+    def obtener_correo(self):
+        return self._correo
+
+
+usuarios = []
+
+def registrar_usuario(nombre, correo):
+    usuario = Usuario(nombre, correo)
+    usuarios.append(usuario)
+    return usuario
+
+def listar_usuarios():
+    return usuarios
