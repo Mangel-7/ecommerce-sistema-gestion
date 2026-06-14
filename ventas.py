95fba401f000dd2d5864a756485c1066099704e0
Mangel-7 <anrogelma@uide.edu.ec>
2026-06-13 19:50:02 -0500

    Update ventas.py

--- ventas.py
+++ ventas.py
@@ -1,5 +1,19 @@
-def generar_venta(carrito):
-    return {
-        "productos": carrito,
-        "total": sum(p["precio"] for p in carrito)
-    }
+class Venta:
+    def __init__(self, usuario, productos):
+        self.usuario = usuario
+        self.productos = productos
+        self.total = self.calcular_total()
+
+    def calcular_total(self):
+        total = 0
+        for producto in self.productos:
+            total += producto.obtener_precio()
+        return total
+
+
+ventas = []
+
+def registrar_venta(usuario, carrito):
+    venta = Venta(usuario, carrito.productos)
+    ventas.append(venta)
+    return venta
