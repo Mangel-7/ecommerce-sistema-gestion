a658efed1b3fc6d4a3701eb12f784f440b70093a
Mangel-7 <anrogelma@uide.edu.ec>
2026-06-13 19:49:01 -0500

    Refactor product handling with Producto class
    Refactor product management by introducing a Producto class and updating agregar_producto and listar_productos functions.

--- productos.py
+++ productos.py
@@ -1,6 +1,31 @@
-def agregar_producto(lista, producto):
-    return lista + [producto]
+import uuid
 
-def listar_productos(lista):
-    for producto in lista:
-        print(producto)
+class Producto:
+    def __init__(self, nombre, precio):
+        self._id = str(uuid.uuid4())
+        self._nombre = nombre
+        self._precio = precio
+
+    def obtener_nombre(self):
+        return self._nombre
+
+    def obtener_precio(self):
+        return self._precio
+
+
+productos = []
+
+def agregar_producto(nombre, precio):
+    try:
+        if precio <= 0:
+            raise ValueError("El precio debe ser mayor a 0")
+
+        producto = Producto(nombre, precio)
+        productos.append(producto)
+        print("✅ Producto agregado correctamente")
+
+    except ValueError as e:
+        print("Error:", e)
+
+def listar_productos():
+    return productos
